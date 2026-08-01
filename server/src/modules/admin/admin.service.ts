74281bb8783486cedb8fa3df10f5c66b255b8964
AtifAziz600 <feralligatrfahim630@gmail.com>
2026-08-01 21:01:34 +0600

    s

--- server/src/modules/admin/admin.service.ts
+++ server/src/modules/admin/admin.service.ts
@@ -7,5 +7,5 @@ export const updateUserStatus = (id: string, status: "ACTIVE" | "SUSPENDED") =>
   prisma.user.update({ where: { id }, data: { status } });
 
 export const getAllGearAdmin = () => prisma.gearItem.findMany({ include: { provider: true, category: true } });
-
+     
 export const getAllRentalsAdmin = () => prisma.rentalOrder.findMany({ include: { items: true, customer: true } });
